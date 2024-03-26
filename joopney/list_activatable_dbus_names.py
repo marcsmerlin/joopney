0384beb647868f9c99616e9ded2c4c463637717b
marcsmerlin <marc.s.merlin@gmail.com>
2024-03-26 08:37:54 -0400

    Sorted output list

--- joopney/list_activatable_dbus_names.py
+++ joopney/list_activatable_dbus_names.py
@@ -1,8 +1,11 @@
 from pprint import pprint
+from typing import List
 
 from connection import ConnectionContext
 from dbus_proxy import DefaultDBusProxy
 
 with ConnectionContext() as connection:
 
-    pprint(DefaultDBusProxy().list_activatable_names(connection))
+    names: List[str] = DefaultDBusProxy().list_activatable_names(connection)
+    names.sort()
+    pprint(names)
