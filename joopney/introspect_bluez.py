1a356e88bff0fb1aa520544ad6082f72c9fc3a4a
marcsmerlin <marc.s.merlin@gmail.com>
2024-03-28 05:58:13 -0400

    Moved commands.

--- joopney/introspect_bluez.py
+++
@@ -1,6 +0,0 @@
-from connection import ConnectionContext
-from bluez_proxy import introspect
-
-with ConnectionContext() as connection:
-
-   print(introspect(connection))
--- joopney/introspect_dbus.py
+++
@@ -1,6 +0,0 @@
-from connection import ConnectionContext
-from dbus_proxy import introspect
-
-with ConnectionContext() as connection:
-
-   print(introspect(connection))
